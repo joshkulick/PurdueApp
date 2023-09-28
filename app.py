0bcc408c5d5c5560bdc0e2dfa23dd780622af21b
joshkulick <Joshkulick.21@gmail.com>
2023-09-28 15:25:11 -0400

    Edits

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 app.secret_key = 'Secret123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://capstonepro:blowfish-orange-840@mysql.ecn.purdue.edu/capstonepro'
 
+# Automatic Email Config for Reset Password
 # Setting it to False disables Flask-SQLAlchemy's modification tracking system
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'your_email@example.com'
+app.config['MAIL_PASSWORD'] = 'BoilerUp10!'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
+mail = Mail(app)
 db = SQLAlchemy(app)
 
 class User(db.Model):
