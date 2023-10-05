3cf3e8e06a4750dc3a81314e26aafb1d5ad7ccc3
joshkulick <Joshkulick.21@gmail.com>
2023-10-05 09:19:52 -0400

    Email Server

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://capstonepro:blowfish-or
 # Automatic Email Config for Reset Password
 # Setting it to False disables Flask-SQLAlchemy's modification tracking system
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'PurdueCapstone@gmail.com'
-app.config['MAIL_PASSWORD'] = 'BoilerUp!'
+app.config['MAIL_PASSWORD'] = 'vbla evma tqpz umof'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
